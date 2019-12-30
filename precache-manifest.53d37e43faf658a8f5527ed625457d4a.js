bfe73effdfe7cd80615f4bc008607694bfded0db
Jon B <jonlondontech@gmail.com>
2019-12-30 11:19:29 +0000

    Updates

---
+++ precache-manifest.53d37e43faf658a8f5527ed625457d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f52ce98496daca27449fdd231a696e0",
+    "url": "/Validation-form/index.html"
+  },
+  {
+    "revision": "0e85d6d3c3d5bbe21b7d",
+    "url": "/Validation-form/static/css/main.22348bb9.chunk.css"
+  },
+  {
+    "revision": "37657b43ff326c870bb9",
+    "url": "/Validation-form/static/js/2.2f6f8062.chunk.js"
+  },
+  {
+    "revision": "0e85d6d3c3d5bbe21b7d",
+    "url": "/Validation-form/static/js/main.2e3f9c46.chunk.js"
+  },
+  {
+    "revision": "a024bd0c982585bbcaed",
+    "url": "/Validation-form/static/js/runtime~main.68049683.js"
+  }
+]);
